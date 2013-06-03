2b8b2e89a0684f4375b4aabbaf5cbd13604cdf8f
Kelly Redding <kelly@kellyredding.com>
2013-06-03 15:56:49 -0500

    modify test helpers assertion methods to use Assert's `with_backtrace`
    This helper was added in Assert v2.2.0 (redding/assert#133). Using
    this helpers allows the results generated in the test helpers to have
    the caller's backtrace, which is more intuitive for the user.
    
    Notice, too, that `with_backtrace` is used in the context methods
    used to test that `with_backtrace` is being used in the test helpers -
    SO META!

--- lib/mr/test_helpers.rb
+++ lib/mr/test_helpers.rb
@@ -12,53 +12,61 @@ module MR::TestHelpers
   # a third arg isn't passed, the intent is to only check that it was saved.
 
   def assert_field_saved(model, field_name, *args)
-    value, check_value = args[0], !args.empty?
+    with_backtrace(caller) do
+      value, check_value = args[0], !args.empty?
 
-    if !model || !field_name
-      raise ArgumentError, "a model and field name must be provided"
-    end
+      if !model || !field_name
+        raise ArgumentError, "a model and field name must be provided"
+      end
 
-    saved = model.send(:record).saved_attributes || {}
-    has_key  = saved.key?(field_name.to_sym)
-    saved_as = saved[field_name.to_sym]
+      saved = model.send(:record).saved_attributes || {}
+      has_key  = saved.key?(field_name.to_sym)
+      saved_as = saved[field_name.to_sym]
 
-    if check_value
-      desc = "Expected #{field_name.inspect} was saved as #{value.inspect}"
-      assert has_key && value == saved_as, desc
-    else
-      desc = "Expected #{field_name.inspect} was saved"
-      assert has_key && !saved_as.nil?, desc
+      if check_value
+        desc = "Expected #{field_name.inspect} was saved as #{value.inspect}"
+        assert has_key && value == saved_as, desc
+      else
+        desc = "Expected #{field_name.inspect} was saved"
+        assert has_key && !saved_as.nil?, desc
+      end
     end
   end
 
   def assert_not_field_saved(model, field_name, *args)
-    value, check_value = args[0], !args.empty?
+    with_backtrace(caller) do
+      value, check_value = args[0], !args.empty?
 
-    if !model || !field_name
-      raise ArgumentError, "a model and field name must be provided"
-    end
+      if !model || !field_name
+        raise ArgumentError, "a model and field name must be provided"
+      end
 
-    saved = model.send(:record).saved_attributes || {}
-    has_key  = saved.key?(field_name.to_sym)
-    saved_as = saved[field_name.to_sym]
+      saved = model.send(:record).saved_attributes || {}
+      has_key  = saved.key?(field_name.to_sym)
+      saved_as = saved[field_name.to_sym]
 
-    if check_value
-      desc = "Expected #{field_name.inspect} was not saved as #{value.inspect}"
-      assert !has_key || value != saved_as, desc
-    else
-      desc = "Expected #{field_name.inspect} was not saved"
-      assert !has_key, desc
+      if check_value
+        desc = "Expected #{field_name.inspect} was not saved as #{value.inspect}"
+        assert !has_key || value != saved_as, desc
+      else
+        desc = "Expected #{field_name.inspect} was not saved"
+        assert !has_key, desc
+      end
     end
   end
 
   def assert_destroyed(model)
-    desc = "Expected the model to be destroyed"
-    assert model.send(:record).destroyed?, desc
+    with_backtrace(caller) do
+      desc = "Expected the model to be destroyed"
+      assert model.send(:record).destroyed?, desc
+    end
   end
 
   def assert_not_destroyed(model)
-    desc = "Expected the model to not be destroyed"
-    assert !model.send(:record).destroyed?, desc
+    with_backtrace(caller) do
+      desc = "Expected the model to not be destroyed"
+      assert !model.send(:record).destroyed?, desc
+    end
   end
 
 end
--- test/unit/test_helpers_tests.rb
+++ test/unit/test_helpers_tests.rb
@@ -6,16 +6,24 @@ module MR::TestHelpers
 
   class FakeContext
     include MR::TestHelpers
-    attr_reader :assertions
+    attr_reader :assertions, :default_backtrace
     def initialize
       @assertions = []
+      @default_backtrace = 'with_backtrace not used'
+      @current_backtrace = @default_backtrace
     end
     def run(*args, &block)
       self.instance_exec(*args, &block)
     end
-    def assert(assertion, *args)
-      @assertions << assertion
+    def assert(result, *args)
+      @assertions << Assertion.new(result, @current_backtrace)
     end
+    def with_backtrace(bt, &block)
+      @current_backtrace = bt
+      block.call
+      @current_backtrace = @default_backtrace
+    end
+    Assertion = Struct.new(:result, :backtrace)
   end
 
   class BaseTests < Assert::Context
@@ -27,56 +35,49 @@ module MR::TestHelpers
     end
     subject{ @context }
 
+    def last_assertion
+      @context.assertions.last
+    end
+
+    def assert_result_is(*args, &block)
+      exp_result = args.shift
+      @context.run(*args, &block)
+      with_backtrace(caller) do
+        desc = "Expected the assertion to be `#{exp_result}`, not `#{last_assertion.result}"
+        assert_equal exp_result, last_assertion.result, desc
+        desc = "Expected the helper to use `with_backtrace`"
+        assert_not_equal @context.default_backtrace, last_assertion.backtrace, desc
+      end
+    end
+
     should "check if the model was destroyed with assert_destroyed and " \
            "assert_not_destroyed" do
-      @context.run(@model){|model| assert_destroyed model }
-      assert_equal false, @context.assertions.last
-
-      @context.run(@model){|model| assert_not_destroyed model }
-      assert_equal true,  @context.assertions.last
+      assert_result_is(false, @model){ |m| assert_destroyed m }
+      assert_result_is(true,  @model){ |m| assert_not_destroyed m }
 
       @model.destroy
 
-      @context.run(@model){|model| assert_destroyed model }
-      assert_equal true, @context.assertions.last
-
-      @context.run(@model){|model| assert_not_destroyed model }
-      assert_equal false,  @context.assertions.last
+      assert_result_is(true,  @model){ |m| assert_destroyed m }
+      assert_result_is(false, @model){ |m| assert_not_destroyed m }
     end
 
     should "check if a model's field was saved with assert_field_saved and " \
-           "assert_field_not_saved" do
-      @context.run(@model){|model| assert_field_saved model, :name }
-      assert_equal false, @context.assertions.last
+           "assert_not_field_saved" do
+      assert_result_is(false, @model){ |m| assert_field_saved m, :name }
+      assert_result_is(true,  @model){ |m| assert_not_field_saved m, :name }
 
-      @context.run(@model){|model| assert_field_saved model, :name, 'Test' }
-      assert_equal false, @context.assertions.last
-
-      @context.run(@model){|model| assert_not_field_saved model, :name }
-      assert_equal true, @context.assertions.last
-
-      @context.run(@model){|model| assert_not_field_saved model, :name, 'Test' }
-      assert_equal true, @context.assertions.last
+      assert_result_is(false, @model){ |m| assert_field_saved m, :name, 'Test' }
+      assert_result_is(true,  @model){ |m| assert_not_field_saved m, :name, 'Test' }
 
       @model.save({ :name => 'Joe' })
 
-      @context.run(@model){|model| assert_field_saved model, :name }
-      assert_equal true, @context.assertions.last
+      assert_result_is(true,  @model){ |m| assert_field_saved m, :name }
+      assert_result_is(true,  @model){ |m| assert_field_saved m, :name, 'Joe' }
+      assert_result_is(false, @model){ |m| assert_field_saved m, :name, 'Test' }
 
-      @context.run(@model){|model| assert_field_saved model, :name, 'Joe' }
-      assert_equal true, @context.assertions.last
-
-      @context.run(@model){|model| assert_field_saved model, :name, 'Test' }
-      assert_equal false, @context.assertions.last
-
-      @context.run(@model){|model| assert_not_field_saved model, :name }
-      assert_equal false, @context.assertions.last
-
-      @context.run(@model){|model| assert_not_field_saved model, :name, 'Joe' }
-      assert_equal false, @context.assertions.last
-
-      @context.run(@model){|model| assert_not_field_saved model, :name, 'Test' }
-      assert_equal true, @context.assertions.last
+      assert_result_is(false, @model){ |m| assert_not_field_saved m, :name }
+      assert_result_is(false, @model){ |m| assert_not_field_saved m, :name, 'Joe' }
+      assert_result_is(true,  @model){ |m| assert_not_field_saved m, :name, 'Test' }
     end
 
   end
