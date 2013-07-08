250fa64480d2d70507a6a7428fc94d4f9333c978
jcredding <TempestTTU@gmail.com>
2013-07-08 15:47:04 -0500

    Make sure `FakeRecord`'s can be used with a `Stack`
    This allows using a fake record with a stack, model or record.
    The only change needed was to provide the association's record
    class. With this information and the previous changes to fake
    records, a record stack can easily "walk" a fake record's belongs
    to associations the same way it does for a real record. This adds
    system tests for both record and model stacks to ensure that using
    a fake record works with the 2 stacks.

--- test/support/models/fake_comment_record.rb
+++ test/support/models/fake_comment_record.rb
@@ -1,4 +1,5 @@
 require 'mr/fake_record'
+require 'test/support/models/fake_user_record'
 
 class FakeCommentRecord
   include MR::FakeRecord
@@ -6,5 +7,7 @@ class FakeCommentRecord
   attribute :message,  :string
   attribute :user_id,  :integer
   attribute :favorite, :boolean
+
+  belongs_to :user, 'FakeUserRecord'
 
 end
