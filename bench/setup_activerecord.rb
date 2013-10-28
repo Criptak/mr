20026f8bbab5aa556ad4288f73f51099b99871be
Collin Redding <TempestTTU@gmail.com>
2013-10-28 11:44:18 -0500

    Add a benchmarking script
    This adds a benchmarking script for MR models and read models.
    This may be expanded to more parts of MR in the future. This is
    intended as a simple way to relatively detect changes in speed
    and memory usage of MR. This will be used shortly to detect if
    changes to MR are more or less efficient than they previously
    were. Each piece is benchmarked in as much isolation as possible.

---
+++ bench/setup_activerecord.rb
@@ -0,0 +1,78 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection({
+  :adapter   => 'sqlite3',
+  :database  => ':memory:',
+  :verbosity => 'quiet'
+})
+
+# silence STDOUT
+current_stdout = $stdout.dup
+$stdout = File.new('/dev/null', 'w')
+ActiveRecord::Schema.define(:version => 1) do
+
+  create_table "areas" do |t|
+    t.string  "name"
+    t.boolean "active"
+    t.text    "description"
+  end
+
+  create_table "users" do |t|
+    t.string  "parent_type"
+    t.integer "parent_id"
+    t.string  "name"
+    t.integer "area_id"
+    t.integer "managed_area_id"
+  end
+
+end
+$stdout = current_stdout
+
+class AreaRecord < ActiveRecord::Base
+  include MR::Record
+  self.table_name = 'areas'
+
+  has_one :manager_user, :class_name => 'UserRecord', :foreign_key => 'managed_area_id'
+  has_many :users, :class_name => 'UserRecord', :foreign_key => 'area_id'
+end
+
+class ValidAreaRecord < ActiveRecord::Base
+  include MR::Record
+  self.table_name = 'areas'
+  validates_presence_of :name
+end
+
+class UserRecord < ActiveRecord::Base
+  include MR::Record
+  self.table_name = 'users'
+
+  belongs_to :parent, :polymorphic => true
+  belongs_to :area, :class_name => 'AreaRecord', :foreign_key => 'area_id'
+end
+
+class Area
+  include MR::Model
+  record_class AreaRecord
+  has_one :manager_user, 'User'
+  has_many :users, 'User'
+end
+
+class User
+  include MR::Model
+  record_class UserRecord
+  polymorphic_belongs_to :parent
+  belongs_to :area, 'Area'
+end
+
+class UserWithAreaData
+  include MR::ReadModel
+
+  def self.all
+    scope = UserRecord.scoped
+    scope = scope.select("users.name AS user_name")
+    scope = scope.select("areas.name AS area_name")
+    scope = scope.joins(:area)
+
+    scope.all.map{ |record| self.new(record) }
+  end
+end
