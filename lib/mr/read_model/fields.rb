a8633606d8547474376f3f7fae2d17177a0dd591
Collin Redding <TempestTTU@gmail.com>
2013-10-29 14:56:26 -0500

    Add new interface for read model fields
    This adds a new mixin `MR::ReadModel::Fields`. This mixin will
    be used to update read models to provide an API for specifying
    the fields that a read model should have. The mixin works with
    the `Querying` mixin to automatically add `select` statements
    to the read models relation for each field. This makes it
    simple to add fields and select out there values. Furthermore,
    fields will now type-cast there values based on the `type` that
    is used when specifying the field. This value is memoized so
    that read models to not repetitively type-cast there values.
    This doesn't make any changes to the `MR::ReadModel` mixin yet,
    this is just setup for changing it. This should make read models
    more useful and also allow them to eventually work with an
    `MR::Factory`.
    
    Related to #88

---
+++ lib/mr/read_model/fields.rb
@@ -0,0 +1,115 @@
+require 'date'
+require 'time'
+require 'ns-options/boolean'
+
+require 'mr/read_model/data'
+require 'mr/read_model/querying'
+
+module MR; end
+module MR::ReadModel
+
+  module Fields
+
+    def self.included(klass)
+      klass.class_eval do
+        include MR::ReadModel::Data
+        include MR::ReadModel::Querying
+        extend ClassMethods
+      end
+    end
+
+    module ClassMethods
+
+      def fields
+        @fields ||= MR::ReadModel::FieldSet.new
+      end
+
+      def field(name, type, column = nil, &column_provider)
+        fields.add(name, type, self)
+        if column
+          select("#{column} AS #{name}")
+        elsif column_provider
+          select{ |args| "#{column_provider.call(args)} AS #{name}" }
+        end
+      rescue BadFieldTypeError => exception
+        raise ArgumentError, exception.message
+      end
+
+    end
+
+  end
+
+  class FieldSet
+
+    def initialize
+      @fields = []
+    end
+
+    def find(name)
+      @fields.detect{ |f| f.name == name.to_s }
+    end
+
+    def add(name, type, model_class = nil)
+      @fields << Field.new(name, type).tap do |field|
+        field.define_on(model_class) if model_class
+      end
+    end
+
+  end
+
+  class Field
+
+    TYPE_CAST_PROCS = Hash.new{ |h, k| raise BadFieldTypeError.new(k) }.tap do |h|
+      h[:string]   = proc{ |input| input.to_s }
+      h[:integer]  = proc{ |input| input.to_i }
+      h[:float]    = proc{ |input| input.to_f }
+      h[:datetime] = proc{ |input| DateTime.parse(input.to_s) }
+      h[:time]     = proc{ |input| Time.parse(input.to_s) }
+      h[:date]     = proc{ |input| Date.parse(input.to_s) }
+      h[:boolean]  = proc{ |input| NsOptions::Boolean.new(input.to_s).actual }
+    end.freeze
+
+    attr_reader :name, :type, :value
+    attr_reader :method_name, :ivar_name
+
+    def initialize(name, type)
+      @name = name.to_s
+      @type = type.to_sym
+      @type_cast_proc = TYPE_CAST_PROCS[@type]
+
+      @method_name    = @name
+      @ivar_name      = "@#{@name}"
+      @attribute_name = @name
+    end
+
+    def read(data)
+      type_cast(data[@attribute_name])
+    end
+
+    def define_on(model_class)
+      field = self
+      model_class.class_eval do
+
+        define_method(field.method_name) do
+          instance_variable_get(field.ivar_name) ||
+          instance_variable_set(field.ivar_name, field.read(data))
+        end
+
+      end
+    end
+
+    private
+
+    def type_cast(value)
+      @type_cast_proc.call(value) unless value.nil?
+    end
+
+  end
+
+  class BadFieldTypeError < RuntimeError
+    def initialize(type_key)
+      super "#{type_key.to_s.inspect} is not a valid field type"
+    end
+  end
+
+end
---
+++ test/unit/read_model/fields_tests.rb
@@ -0,0 +1,241 @@
+require 'assert'
+require 'mr/read_model/fields'
+
+require 'mr/factory'
+
+module MR::ReadModel::Fields
+
+  class UnitTests < Assert::Context
+    desc "MR::ReadModel::Fields"
+    setup do
+      @read_model_class = Class.new do
+        include MR::ReadModel::Fields
+        def initialize(data); set_data(data); end
+      end
+    end
+    subject{ @read_model_class }
+
+    should have_imeths :fields, :field
+
+    should "include the ReadModel Data and Querying mixins" do
+      assert_includes MR::ReadModel::Data, subject
+      assert_includes MR::ReadModel::Querying, subject
+    end
+
+    should "return a FieldSet using `fields`" do
+      fields = subject.fields
+      assert_instance_of MR::ReadModel::FieldSet, fields
+      assert_same fields, subject.fields
+    end
+
+    should "add a field to the FieldSet using `field`" do
+      subject.field :test, :string
+      field = subject.fields.find(:test)
+
+      assert_instance_of MR::ReadModel::Field, field
+      assert_equal 'test',  field.name
+      assert_equal :string, field.type
+    end
+
+    should "define a reader method for the field using `field`" do
+      subject.field :test, :string
+      data = { 'test' => 'something' }
+      read_model = subject.new(data)
+
+      assert_respond_to :test, read_model
+      assert_equal 'something', read_model.test
+    end
+
+    should "add a select to its relation when `field` is passed select args" do
+      subject.field :test, :string, 'some_table.some_column'
+
+      assert_equal 1, subject.relation.selects.size
+      expression = subject.relation.selects.first
+      assert_equal 'some_table.some_column AS test', expression.args.first
+    end
+
+    should "add a select to its relation when `field` is passed a block" do
+      subject.field(:test, :string){ |column| column }
+
+      assert_equal 1, subject.relation.selects.size
+      expression = subject.relation.selects.first
+      expected = 'some_table.some_column AS test'
+      assert_equal expected, expression.block.call('some_table.some_column')
+    end
+
+    should "raise an ArgumentError when `field` is passed an invalid field type" do
+      assert_raises(ArgumentError){ subject.field :test, :invalid }
+    end
+
+  end
+
+  class FieldSetTests < UnitTests
+    desc "FieldSet"
+    setup do
+      @field_set = MR::ReadModel::FieldSet.new
+    end
+    subject{ @field_set }
+
+    should have_imeths :find, :add
+
+  end
+
+  class FieldTests < UnitTests
+    desc "Field"
+    setup do
+      @field = MR::ReadModel::Field.new(:test, :string)
+    end
+    subject{ @field }
+
+    should have_readers :name, :type
+    should have_readers :method_name, :ivar_name
+    should have_imeths :read, :define_on
+
+    should "know it's name and type" do
+      assert_equal 'test',  subject.name
+      assert_equal :string, subject.type
+    end
+
+    should "know it's method name and ivar name" do
+      assert_equal 'test',  subject.method_name
+      assert_equal '@test', subject.ivar_name
+    end
+
+    should "read a value from passed-in data using `read`" do
+      data = { 'test' => 'something' }
+      assert_equal 'something', subject.read(data)
+    end
+
+    should "return `nil` when passed-in data's value is `nil` using `read`" do
+      data = { 'test' => nil }
+      assert_equal nil, subject.read(data)
+    end
+
+    should "define a reader method on an object using `define_on`" do
+      subject.define_on(@read_model_class)
+      read_model = @read_model_class.new({ 'test' => 'something' })
+
+      assert_respond_to subject.method_name, read_model
+      value = read_model.send(subject.method_name)
+      assert_same value, read_model.send(subject.method_name)
+    end
+
+    should "raise a bad field type error when built with an invalid field type" do
+      assert_raises(MR::ReadModel::BadFieldTypeError) do
+        MR::ReadModel::Field.new(:test, :invalid)
+      end
+    end
+
+  end
+
+  class StringFieldTests < FieldTests
+    desc "of type string"
+    setup do
+      @field = MR::ReadModel::Field.new(:test, :string)
+    end
+
+    should "type cast data values using `read`" do
+      assert_equal 'string', subject.read({ 'test' => 'string' })
+    end
+
+  end
+
+  class IntegerFieldTests < FieldTests
+    desc "of type integer"
+    setup do
+      @field = MR::ReadModel::Field.new(:test, :integer)
+    end
+
+    should "type cast data values using `read`" do
+      value = MR::Factory.integer
+      assert_equal value, subject.read({ 'test' => value.to_s })
+
+      value = MR::Factory.float
+      assert_equal value.to_i, subject.read({ 'test' => value.to_s })
+    end
+
+  end
+
+  class FloatFieldTests < FieldTests
+    desc "of type float"
+    setup do
+      @field = MR::ReadModel::Field.new(:test, :float)
+    end
+
+    should "type cast data values using `read`" do
+      value = MR::Factory.float
+      actual = subject.read({ 'test' => value.to_s })
+      assert_instance_of Float, actual
+      assert_equal value.to_s, actual.to_s
+
+      value = MR::Factory.integer
+      assert_equal value.to_f, subject.read({ 'test' => value.to_s })
+    end
+
+  end
+
+  class DateTimeFieldTests < FieldTests
+    desc "of type datetime"
+    setup do
+      @field = MR::ReadModel::Field.new(:test, :datetime)
+    end
+
+    should "type cast data values using `read`" do
+      value = MR::Factory.datetime
+      actual = subject.read({ 'test' => value.to_s })
+      assert_instance_of DateTime, actual
+      assert_equal value.to_s, actual.to_s
+    end
+
+  end
+
+  class TimeFieldTests < FieldTests
+    desc "of type time"
+    setup do
+      @field = MR::ReadModel::Field.new(:test, :time)
+    end
+
+    should "type cast data values using `read`" do
+      value = MR::Factory.time
+      actual = subject.read({ 'test' => value.to_s })
+      assert_instance_of Time, actual
+      assert_equal value.to_s, actual.to_s
+    end
+
+  end
+
+  class DateFieldTests < FieldTests
+    desc "of type date"
+    setup do
+      @field = MR::ReadModel::Field.new(:test, :date)
+    end
+
+    should "type cast data values using `read`" do
+      value = MR::Factory.date
+      actual = subject.read({ 'test' => value.to_s })
+      assert_instance_of Date, actual
+      assert_equal value.to_s, actual.to_s
+    end
+
+  end
+
+  class BooleanFieldTests < FieldTests
+    desc "of type boolean"
+    setup do
+      @field = MR::ReadModel::Field.new(:test, :boolean)
+    end
+
+    should "type cast data values using `read`" do
+      assert_equal true,  subject.read({ 'test' => 'true' })
+      assert_equal true,  subject.read({ 'test' => '1' })
+      assert_equal true,  subject.read({ 'test' => 't' })
+      assert_equal true,  subject.read({ 'test' => 'T' })
+
+      assert_equal false, subject.read({ 'test' => 'false' })
+      assert_equal false, subject.read({ 'test' => '0' })
+      assert_equal false, subject.read({ 'test' => 'f' })
+      assert_equal false, subject.read({ 'test' => 'F' })
+    end
+  end
+
+end
